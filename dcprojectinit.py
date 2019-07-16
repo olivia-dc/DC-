c39da6c19bb37ef0459b7b735c60d4e9531e344b
olivia-dc <52371519+olivia-dc@users.noreply.github.com>
2019-07-16 14:15:04 +0800

    Add files via upload

--- dcprojectinit.py
+++ dcprojectinit.py
@@ -8,9 +8,12 @@ import datetime
 2. expdb & impdb from odi base to new work rep script
 3. new dc stage and target db user script
 4. generate .sql and .bat file
+<project_short> control name such as : {xxx}_ls_src
+<ls_gs> control such as : {xxx}_{ls/gs}_src
+<dc_user_base> control bat execute at o46g4 or c1815u1/u2 and tablespace datafile location
 """
 class InitDCProject:
-       def __init__(self,project_short,ls_gs,work_rep_base,dc_user_base):
+       def __init__(self,project_short,ls_gs,dc_user_base):
               self.__str_date = datetime.datetime.now().strftime("%Y%m%d")
               self.__project_short = project_short
               self.__ls_gs = ls_gs
@@ -22,9 +25,9 @@ class InitDCProject:
               self.__dc_schema_init_file_name = '03_dc_schema_init_script.sql'
               self.__dc_schema_init_bat = '03_dc_schema_init_script.bat'
 
-              work_rep_init_bat_script = 'sqlplus dc/dc@{work_rep_base} @01_work_rep_init_script.sql'
+              work_rep_init_bat_script = 'sqlplus dc/dc@o46g4 @01_work_rep_init_script.sql'
               dc_schema_init_bat_script = 'sqlplus dc/dc@{dc_user_base} @03_dc_schema_init_script.sql'
-              self.__work_rep_bat = work_rep_init_bat_script.format(work_rep_base= work_rep_base)
+              self.__work_rep_bat = work_rep_init_bat_script
               self.__dc_schema_bat =dc_schema_init_bat_script.format(dc_user_base= dc_user_base)
 
 # IS AND GS have different odi base
@@ -37,17 +40,17 @@ class InitDCProject:
 
               # execute at work_rep_base
               self.__create_work_rep_sql = """
-              create user  rep_{project_short}  identified by  rep_{project_short}  ; 
+              create user rep_{project_short}  identified by  rep_{project_short} ; 
               grant connect,resource,create session,create view,create synonym,
                select any table,unlimited tablespace to rep_{project_short}; 
               grant dba to rep_{project_short};  """
               self.__exp_impdb_sql = """ 
               expdp {exp_db}/{exp_db}@o46g4 schemas={exp_db} dumpfile={exp_db}_{date}.dmp logfile={exp_db}_{date}.log directory=data_pump_dir;
-              impdp rep_{project_short}/rep_{project_short}@o46g4 directory=data_pump_dir dumpfile={exp_db}_{date}.dmp logfile={exp_db}_{date}.log remap_schema={exp_db}:rep_{project_short} full=y ignore=y ;
+              impdp rep_{project_short}/rep_{project_short}@o46g4 directory=data_pump_dir dumpfile={exp_db}_{date}.dmp logfile={exp_db}_{date}.log remap_schema={exp_db}:rep_{project_short} full=y ignore=y table_exists_action = replace ;
               """
 
               #execute at dc_user_base
-              self.__create_tablespace_sql =  """ 
+              self.__create_tablespace_sql_dc_user =  """ 
               create tablespace tbs_{project_short} 
               datafile '/oradata/{dc_user_base}/tbs_{project_short}_01.dbf' size 50m autoextend on  ,         
                '/oradata/{dc_user_base}/tbs_{project_short}_02.dbf' size 50m autoextend on    ;   """
@@ -65,7 +68,7 @@ class InitDCProject:
 
        # 1 生成work rep 创建的脚本
        def work_rep_init_script(self):
-              script= self.__create_work_rep_sql.format(project_short=self.__project_short)
+              script=  self.__create_work_rep_sql.format(project_short=self.__project_short)
               return script
               pass
        # 2 生成expdb impdb 的脚本
@@ -75,7 +78,7 @@ class InitDCProject:
               pass
        # 3 生成dc schema 创建的脚本
        def dc_schema_init_script(self):
-              script = self.__create_tablespace_sql.format(project_short = self.__project_short,dc_user_base = self.__dc_user_base) + '\n' + self.__create_stage_user_sql.format(project_short = self.__project_short,ls_gs = self.__ls_gs) + '\n' \
+              script = self.__create_tablespace_sql_dc_user.format(project_short = self.__project_short,dc_user_base = self.__dc_user_base) + '\n' + self.__create_stage_user_sql.format(project_short = self.__project_short,ls_gs = self.__ls_gs) + '\n' \
                      + self.__create_target_user_sql.format(project_short = self.__project_short,ls_gs = self.__ls_gs) + '\n'
               return script
               pass
@@ -111,7 +114,7 @@ if __name__=='__main__':
        dc_user_base = ''
        ls_gs = ''
        # Test
-       init_odi = InitDCProject('POC','LS','o46g4','o46g4')
+       init_odi = InitDCProject('POC','LS','o46g4')
        work_rep_init_script = init_odi.save_work_rep_init_script()
        expdp_impdp_script = init_odi.save_expdp_impdp_script()
        dc_schema_init_script = init_odi.save_dc_schema_init_script()
